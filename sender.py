9a6a2c1694f2cd12ca937a04976fd421531994f4
Leonardo Delgado <sdelgadom@unal.edu.co>
2024-05-01 14:59:33 -0500

    sniffing other hosts

--- sender.py
+++ sender.py
@@ -1,7 +1,7 @@
 from scapy.all import IP, TCP, send
 
 a = IP()
-a.dst = '192.168.2.6'
+a.dst = '192.168.2.8'
 b = TCP(dport=23)
 p = a/b
 send(p)
--- sniffing.py
+++ sniffing.py
@@ -4,4 +4,5 @@ def print_pkt(pkt):
 
 subred = "192.168.2.0/24"
 f = f"(src net {subred}) or (dst net {subred})"
-pkt = sniff(filter=f'tcp and host 192.168.2.8 and port 23 and ({f})',prn=print_pkt, count=3)
+#pkt = sniff(filter=f'tcp dst host 192.168.2.8 and port 23 and ({f})',prn=print_pkt, count=1)
+pkt = sniff(filter=f'tcp and dst host 192.168.2.8',prn=print_pkt, count=0)
